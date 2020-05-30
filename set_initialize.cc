8f5da6891150152ddac847552384c859b73742b2
bliutwo <bliutwo@cs.stanford.edu>
2020-05-29 20:46:41 -0700

    29/05/2020 20:46:41

--- set_initialize.cc
+++ set_initialize.cc
@@ -2,7 +2,26 @@
 #include <set>
 using namespace std;
 
+void output(set<int> s) {
+	set<int>::iterator it;
+	for (it = s.begin(); it != s.end(); it++) {
+		cout << *it << endl;
+	}
+}
+
+void output11(set<int> s) {
+	for (const auto& x : s) {
+		cout << x << endl;
+	}
+}
+
 int main() {
-	
+	set<int> s;
+	s.insert(0);
+	s.insert(2);
+	s.insert(69);
+	s.insert(-1);
+	output(s);
+	output11(s);
 	return 0;
 }
