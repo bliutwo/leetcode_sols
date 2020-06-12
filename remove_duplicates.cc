20acd9c02dd9e8dd240bd8a2cba9c5df2a6c7c92
bliutwo <bliutwo@cs.stanford.edu>
2020-06-12 11:44:33 -0700

    12/06/2020 11:44:33

--- remove_duplicates.cc
+++ remove_duplicates.cc
@@ -1,25 +1,34 @@
-#include "gtest/gtest.h"
 #include <bits/stdc++.h>
+#include "gtest/gtest.h"
 using namespace std;
 
-int example_function(int input) {
-    return input;
+int remove_duplicates(string& s) {
+    if (s.empty()) return s;
+    for (int i = 0; i < s.length() - 1; i++) {
+        char a = s[i];
+        char b = s[i+1];
+        if (a == b) {
+            s.erase(i, 2);
+            i = -1;
+        }
+    }
+    return s;
 }
 
 struct Test1 : ::testing::Test {
-    input = 1;
-    output = 1;
+    string input = "aaab";
+    string output = "ab";
 };
 
 struct Test2 : ::testing::Test {
-    input = 0;
-    output = 1;
+    string input = "aa";
+    string output = "";
 };
 
 TEST_F(Test1, UnitTest1) {
-    ASSERT_EQ(example_function(input), output);
+    ASSERT_EQ(remove_duplicates(input), output);
 }
 
 TEST_F(Test2, UnitTest2) {
-    ASSERT_EQ(example_function(input), output);
+    ASSERT_EQ(remove_duplicates(input), output);
 }
