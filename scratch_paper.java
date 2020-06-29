836d6fe2e9ebe73dcde77233667134ebe175d00b
bliutwo <bliutwo@cs.stanford.edu>
2020-06-28 21:27:42 -0700

    28/06/2020 21:27:42

--- scratch_paper.java
+++ scratch_paper.java
@@ -1,5 +1,9 @@
-public class Main {
+public class scratch_paper {
     public static void main(String[] args) {
         System.out.println("This will be printed");
+        int a = 0;
+        int b = 1;
+        int c;
+        if (a > b) { c = 0; }
     }
 }
